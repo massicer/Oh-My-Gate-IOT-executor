95fd57796d603df117ccc2b27e8631f21291c772
Massimiliano Ceriani <massimilianoceriani08@gmail.com>
2020-12-28 15:14:10 +0100

    add: gpio adapter

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/massicer/Oh-My-Gate-IOT-executor
 go 1.15
 
 require (
-	cloud.google.com/go v0.73.0
 	cloud.google.com/go/pubsub v1.9.1
 	github.com/apsdehal/go-logger v0.0.0-20190515212710-b0d6ccfee0e6
+	github.com/stianeikeland/go-rpio/v4 v4.4.0
 )
--- internal/iot_adapter/adapter.go
+++ internal/iot_adapter/adapter.go
@@ -1,9 +1,20 @@
 package iot_adapter
 
+
 import (
+	"os"
 	"fmt"
+	"errors"
+	"time"
 	"io"
 	"github.com/massicer/Oh-My-Gate-IOT-executor/internal/entities"
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+const (
+	STANDARD_OUT_ADAPTER = "standard_out"
+	GPIO_ADAPTER = "gpio"
+	SLEEP_TIME_IN_SECONDS = 3 * time.Second
 )
 
 
@@ -15,7 +26,40 @@ type Sdout_iot_adapter struct {
 	W io.Writer
 }
 
+type GpioAdapter struct {
+
+}
+
 func (s *Sdout_iot_adapter) Open(msg entities.OpenMessage) error {
 	fmt.Fprintf(s.W, "Going to open iot device with id: %d", msg.Id)
     return nil
+}
+
+func (s *GpioAdapter) Open(msg entities.OpenMessage) error {
+	defer rpio.Close()
+	err := rpio.Open()
+
+	if err != nil {
+		return err
+	}
+
+	pin := rpio.Pin(msg.Id)
+	pin.Output()
+	pin.High()  
+	time.Sleep(SLEEP_TIME_IN_SECONDS)
+	pin.Low()  
+	return nil
+}
+
+func GetAdapter(adapter_type string) (Iot_adapter, error) {
+
+  switch adapter_type{
+  case STANDARD_OUT_ADAPTER:
+	return &Sdout_iot_adapter{W: os.Stdout}, nil
+  case GPIO_ADAPTER:
+	return &GpioAdapter{}, nil
+
+	default:
+		return nil, errors.New("Adapter type not found")
+  }
 }
